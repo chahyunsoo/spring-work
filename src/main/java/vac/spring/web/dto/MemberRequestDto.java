5282c1b19af6d01548780efa42806696109b3a71
chahyunsoo <chahs1012@naver.com>
2024-01-05 20:44:52 +0900

    feat : Member DTO 생성

---
+++ src/main/java/vac/spring/web/dto/MemberRequestDto.java
@@ -0,0 +1,19 @@
+package vac.spring.web.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+public class MemberRequestDto {
+    @Getter
+    public static class JoinDto {
+        String name;
+        Integer gender;
+        Integer birthYear;
+        Integer birthMonth;
+        Integer birthDay;
+        String address;
+        String specAddress;
+        List<Long> preferCategory;
+    }
+}
---
+++ src/main/java/vac/spring/web/dto/MemberResponseDto.java
@@ -0,0 +1,19 @@
+package vac.spring.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class MemberResponseDto {
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class JoinResultDto {
+        Long memberId;
+        LocalDateTime createdAt;
+    }
+}
