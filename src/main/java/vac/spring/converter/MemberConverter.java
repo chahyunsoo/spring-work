e3f626dea4f35c658c37f463f3aade86732e15c6
chahyunsoo <chahs1012@naver.com>
2024-01-05 20:45:13 +0900

    feat : Member Converter 생성 구현

---
+++ src/main/java/vac/spring/converter/MemberConverter.java
@@ -0,0 +1,34 @@
+package vac.spring.converter;
+
+import vac.spring.domain.Member;
+import vac.spring.domain.enums.Gender;
+import vac.spring.web.dto.MemberRequestDto;
+import vac.spring.web.dto.MemberResponseDto;
+
+import java.time.LocalDateTime;
+
+public class MemberConverter {
+    public static MemberResponseDto.JoinResultDto toJoinResultDto(Member member) {
+        return MemberResponseDto.JoinResultDto.builder()
+                .memberId(member.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static Member toMember(MemberRequestDto.JoinDto request) {
+        Gender gender = null;
+
+        switch (request.getGender()) {
+            case 1:
+                gender = Gender.MALE;
+                break;
+            case 2:
+                gender = Gender.FEMALE;
+                break;
+            case 3:
+                gender = Gender.NONE;
+                break;
+        }
+
+    }
+}
